71b6559cf6a2867e1ba06c60004543d767732be9
Sergey <dir@syakovlev.ru>
2019-05-28 16:45:00 +0500

    Add file. Add struct in file

---
+++ Cryptocurrency.swift
@@ -0,0 +1,16 @@
+//
+//  Cryptocurrency.swift
+//  
+//
+//  Created by Sergey on 22/05/2019.
+//
+
+import Foundation
+
+
+struct Cryptocurrency {
+
+    let symbol: String
+    let name: String
+    let priceUsdLabel: String
+}
